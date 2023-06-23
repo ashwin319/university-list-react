c87226ca8ca42f72fce01b920c44c3c9b3909a72
ashwin319 <ashwin.samtani@gmail.com>
2023-06-23 19:29:21 -0400

    add translation functionality

--- src/components/UniSearch/UniSearch.js
+++ src/components/UniSearch/UniSearch.js
@@ -1,17 +1,35 @@
 import './UniSearch.css'
+import { useTranslation } from 'react-i18next';
+import { useEffect } from 'react';
 
 function UniSearch({ search, updateSearch }) {
+    const {t, i18n} = useTranslation();
+
     const onSearchChange = (event) => {
         updateSearch(event.target.value)
     }
 
+    useEffect(() => {
+        i18n.changeLanguage('es')
+    }, [])
+
+    const changeLanguage = (evt) => {
+        const language = evt.target.value;
+        i18n.changeLanguage(language);
+    }
+
     return (
         <div className="uni-search">
-            Search: 
+            <select onChange={changeLanguage}>
+                <option value="en">English</option>
+                <option value="es">Espanol</option>
+            </select>
+
+            {t('uniSearch.search.label')}: 
             <input 
                 onChange={onSearchChange}
                 value={search}
-                placeholder="Search for your uni here" 
+                placeholder={t('uniSearch.search.placeholder')}
             />
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import './i18n.js'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
